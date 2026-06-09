bccee92ded459f520c5f9bfe9f4cb0d72d44de4e
gouravsaini1308 <gouravsaini1308@gmail.com>
2026-06-09 17:00:11 +0530

    first commit

---
+++ src/utils.py
@@ -0,0 +1,81 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+'''
+1. file_path → where you want to save the object.
+2. obj → the object you want to save (model, preprocessor, scaler, etc.).
+3. os.path.dirname() extracts the folder name from the complete path.
+4. Why is this useful in an ML project?
+    -After training:
+    preprocessor = ColumnTransformer(...)
+    model = RandomForestRegressor(...)
+
+    -You don't want to retrain every time.
+
+    -So you save them:
+    save_object(
+        "artifacts/preprocessor.pkl",
+        preprocessor
+    )
+
+    save_object(
+        "artifacts/model.pkl",
+        model
+    )
+'''
+
+def evaluate_models(X_train,y_train,X_test,y_test,models, params):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_accuracy = r2_score(y_train, y_train_pred)
+            test_model_accuracy = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_accuracy
+
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
