67532900a789738251515ae9d8f934fe1da78ef1
gouravsaini1308 <gouravsaini1308@gmail.com>
2026-06-27 01:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 
+
+
+
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
